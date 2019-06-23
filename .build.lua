a862ef19549f9d8ed2a2851d07376be43f690f91
nift4 <36458596+nift4@users.noreply.github.com>
2019-06-23 13:54:18 +0200

    Update .build.lua

--- .build.lua
+++ .build.lua
@@ -2,6 +2,7 @@ if _HOST then howlci.log("info", "Host: " .. _HOST) end
 if _CC_VERSION then howlci.log("info", "CC Version" .. _CC_VERSION) end
 if _MC_VERSION then howlci.log("info", "MC Version" .. _MC_VERSION) end
 if _LUAJ_VERSION then howlci.log("info", "LuaJ Version " .. _LUAJ_VERSION) end
+howlci.log("info","Build Mode: " .. howlci.getEnv("TRAVIS_BRANCH"))
 
 shell.run("pastebin","run","0QUAEvtK","get","SquidDev","703e2f46ce68c2ca158673ff0ec4208c/Howl.min.lua","Howl")
 local handle = fs.open(".howl/settings.lua", "w")
